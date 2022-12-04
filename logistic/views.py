adaf2a26b769100717af0a54ce514b80f35c161a
mhfhfmn <kyrsivom@gmail.com>
2022-12-04 16:14:05 +0300

    Fix imports

--- logistic/views.py
+++ logistic/views.py
@@ -1,10 +1,8 @@
 from rest_framework.viewsets import ModelViewSet
 from django_filters.rest_framework import DjangoFilterBackend
-from rest_framework.filters import SearchFilter, OrderingFilter
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.filters import SearchFilter
 from logistic.models import Product, Stock, StockProduct
 from logistic.serializers import ProductSerializer, StockSerializer, ProductPositionSerializer
-from django.shortcuts import render
 
 
 class ProductViewSet(ModelViewSet):
